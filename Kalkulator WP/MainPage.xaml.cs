fa24621833051e0cd6a387bc4b4470a01abba8af
Tena Vilček <vilcek.tena@gmail.com>
2017-11-12 00:04:06 +0100

    finished version

---
+++ Kalkulator WP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Kalkulator_WP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+              
+
+
+        private  void button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button) sender;
+            int op1;
+            int op2;
+            bool result = int.TryParse(textBox.Text, out op1 );
+            bool result2 = int.TryParse(textBox1.Text, out op2);
+
+            textBox2.Text = (op1 + op2).ToString();
+
+        }
+    }
+}
